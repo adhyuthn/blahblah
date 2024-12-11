32260ab09bb0671316c24744b66a7de431299e06
Adhyuth Narayan <98572686+adhyuthn@users.noreply.github.com>
2024-12-12 03:17:54 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -12,5 +12,7 @@ time_text    = last_entry[0]
 heading_text = last_entry[1]
 content_text = last_entry[2]
 
+print("RESPONSE:\n", response)
+
 with open("README.md", "a") as fd:
-    fd.write(f"### {heading_text}\n**Timestamp: {time_text}**\n{content_text}\n\n")
+    fd.write(f"### {heading_text}\nTimestamp: {time_text}\n{content_text}\n\n")
