889fd6f1b55b759e303895a9da9300673ca0c15c
Adhyuth Narayan <98572686+adhyuthn@users.noreply.github.com>
2024-12-12 01:38:07 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,7 +1,15 @@
-import random
-import datetime
+import requests
+import pprint
 
-fd = open("README.md", "a")
-text = "## NEW " + str(datetime.datetime.now())
-fd.write(text)
-fd.close()
+API_KEY = 'AIzaSyBRlNH70z3tuBVz5BKlAMNMOo2gPnYZ-84'
+spreadsheet_id = '1LKY0vyVjpCy0cozdtQnI56wOQFqSqlI18_5I-Wxe8GM'
+range_to_read = 'A1:D5'
+url = f'https://sheets.googleapis.com/v4/spreadsheets/{spreadsheet_id}/values/{range_to_read}?key={API_KEY}'
+
+response = requests.get(url)
+last_entry = response.json()["values"][-1]
+heading_text = last_entry[1]
+content_text = last_entry[2]
+
+with open("README.md", "a") as fd:
+    fd.write(f"### {heading_text}\n{content_text}\n\n")
