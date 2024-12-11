dd92c7a2cb6fa5fc5d7849505305e03155baef22
Adhyuth Narayan <98572686+adhyuthn@users.noreply.github.com>
2024-12-12 03:38:48 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,10 +1,11 @@
 import requests
 import pprint
+import os
 
-API_KEY = 'AIzaSyBRlNH70z3tuBVz5BKlAMNMOo2gPnYZ-84'
-spreadsheet_id = '1LKY0vyVjpCy0cozdtQnI56wOQFqSqlI18_5I-Wxe8GM'
+API_KEY = os.environ["API_KEY_SHEETS"]
+SPREADSHEET_ID = os.environ["SPREADSHEET_ID_SHEETS"]
 range_to_read = 'A2:C'
-url = f'https://sheets.googleapis.com/v4/spreadsheets/{spreadsheet_id}/values/{range_to_read}?key={API_KEY}'
+url = f'https://sheets.googleapis.com/v4/spreadsheets/{SPREADSHEET_ID}/values/{range_to_read}?key={API_KEY}'
 
 response = requests.get(url)
 last_entry = response.json()["values"][-1]
@@ -15,4 +16,4 @@ content_text = last_entry[2]
 print("RESPONSE:\n", response.json())
 
 with open("README.md", "a") as fd:
-    fd.write(f"### {heading_text}\nTimestamp: {time_text}\n{content_text}\n\n")
+    fd.write(f"### {heading_text}\nTimestamp: {time_text} \n\n{content_text}\n\n")
